463d9413e73a4e085ef115598d08b709f8bb51a8
Vinícius de Ataídes Bittencourt <vinisataides@gmail.com>
2020-08-26 16:56:02 -0300

    Setting up Migrations

---
+++ src/database/migrations/1598471708288-CriarUsuarios.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CriarUsuarios1598471708288 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
