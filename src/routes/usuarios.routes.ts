25b459e08169f2d6ee3f614bb4bc60b6fc42c7cb
Vinicius Bittencourt <vinisataides@gmail.com>
2020-09-28 18:43:06 -0300

    Envio de Imagem - Multer

--- src/routes/usuarios.routes.ts
+++ src/routes/usuarios.routes.ts
@@ -1,13 +1,15 @@
 import { request, Router } from "express";
 import { getRepository } from "typeorm";
+import multer from "multer";
 
 import UsuariosController from "../app/controllers/UsuariosController";
 import Usuarios from "../app/models/Usuarios";
 import ensureAthen from "../middlewares/ensureAuthenticated";
+import uploadConfig from "../config/upload";
 
 const usuariosRouter = Router();
 
-usuariosRouter.use(ensureAthen);
+const upload = multer(uploadConfig);
 
 usuariosRouter.post("/", async (req, res) => {
     try {
@@ -29,7 +31,7 @@ usuariosRouter.post("/", async (req, res) => {
     }
 });
 
-usuariosRouter.get("/", async (req, res) => {
+usuariosRouter.get("/", ensureAthen, async (req, res) => {
     const usuariosRepositorio = getRepository(Usuarios);
     const user = await usuariosRepositorio.find();
     // console.log(req.user);
@@ -37,18 +39,23 @@ usuariosRouter.get("/", async (req, res) => {
     return res.status(200).json(user);
 });
 
-usuariosRouter.get("/:id", async (req, res) => {
+usuariosRouter.get("/:id", ensureAthen, async (req, res) => {
     const usuariosRepositorio = getRepository(Usuarios);
     const { id } = req.params;
     const user = await usuariosRepositorio.findOne(id);
     return res.status(200).json(user);
 });
 
-usuariosRouter.delete("/:id", async (req, res) => {
+usuariosRouter.delete("/:id", ensureAthen, async (req, res) => {
     const usuariosRepositorio = getRepository(Usuarios);
     const { id } = req.params;
     await usuariosRepositorio.delete(id);
     return res.status(200).send();
+});
+
+usuariosRouter.patch("/avatar", upload.single("avatar"), async (req, res) => {
+    console.log(req.file);
+    return res.json({ ok: true });
 });
 
 export default usuariosRouter;
